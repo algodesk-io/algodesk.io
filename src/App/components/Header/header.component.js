0f0a3a1c99b6f8223f78de3f9e1debc63a38a967
algodesk-io <admin@algodesk.io>
2021-02-20 11:57:26 +0530

    donate button

--- src/App/components/Header/header.component.js
+++ src/App/components/Header/header.component.js
@@ -4,10 +4,16 @@ import {connect} from "react-redux";
 import { withStyles } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import {Dialog, DialogActions, DialogContent, DialogTitle} from "@material-ui/core";
+import QRCode from 'qrcode.react';
 
 const styles = (theme) => ({
     primaryText: {
         color: theme.palette.primary.dark
+    },
+    dialog: {
+        position: 'absolute',
+        top: 150
     }
 });
 
@@ -15,9 +21,47 @@ const mapStateToProps = state => {
     return { };
 };
 
+function renderModals() {
+    const {classes} = this.props;
+
+    return (<div className='all-modals'>
+        {this.state.showDonateModal ?
+            <Dialog
+                fullWidth={true}
+                maxWidth={"sm"}
+                open={this.state.showDonateModal}
+                classes={{
+                    paper: classes.dialog
+                }}
+            >
+                <DialogContent>
+                    <div className='wallet' style={{marginTop: 10}}>
+                        <div className='wallet-header'>Algorand</div>
+                        <QRCode value={'KJRDDME3ZIFJFB2M6B7ZCMEDAB6BAQLTSQASNV2NUBDCDZFBUPWWJISL5I'} size={256}/>
+                        <div className='wallet-address'>
+                            KJRDDME3ZIFJFB2M6B7ZCMEDAB6BAQLTSQASNV2NUBDCDZFBUPWWJISL5I
+                        </div>
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => {
+                        this.setState({...this.state, showDonateModal: false});
+                    }} color="secondary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog> : ''}
+
+    </div>);
+}
+
 class Header extends React.Component {
     constructor(props) {
         super(props);
+        this.renderModals = renderModals.bind(this);
+        this.state = {
+            showDonateModal: false
+        };
     }
 
     render() {
@@ -33,7 +77,7 @@ class Header extends React.Component {
                             <span className={classes.primaryText}>Algodesk.io</span>
                         </div>
                     </Grid>
-                    <Grid item xs={2} sm={2} md={2} xl={2}>
+                    <Grid item xs={4} sm={4} md={4} xl={4}>
                             <Button variant="text"
                                     style={{marginTop: 10}}
                                     onClick={() => {
@@ -50,10 +94,24 @@ class Header extends React.Component {
                         >
                             Report issues
                         </Button>
+
+                        <Button variant="contained"
+                                color={"primary"}
+                                size={"small"}
+                                style={{marginTop: 10, marginLeft: 10}}
+                                onClick={() => {
+                                    console.log(42342);
+                                    this.setState({...this.state, showDonateModal: true});
+                                }}
+                        >
+                            Donate
+                        </Button>
+
                     </Grid>
                 </Grid>
 
             </div>
+            {this.renderModals()}
         </div>);
     }
 }
